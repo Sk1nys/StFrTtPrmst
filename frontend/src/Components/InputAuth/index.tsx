87a31e8af49c192f03891a40502cd03e03a84754
Sk1nys <vkirill680@gmail.com>
2024-10-17 00:20:19 +0300

    neche

---
+++ frontend/src/Components/InputAuth/index.tsx
@@ -0,0 +1,8 @@
+import styles from "..//InputAuth//InputAuth.module.css"
+const InputAuth = () => {
+  return (
+   <input type="text" className={styles.InputAuth}/>
+  )
+}
+
+export default InputAuth
