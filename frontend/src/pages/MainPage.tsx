f7d6fe7f8936014c5feb0eca865e998790d7b42f
m4kyv <braun-erik@inbox.ru>
2024-10-18 22:03:33 +0200

    add animation to description on 1st screen

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,9 +1,12 @@
 import Benefits from '../Components/Benefits';
+// import Footer from '../Components/Footer';
 import Header from '../Components/Header'
 import MainPageSlider from '../Components/MainPageSlider'
-import styles from './styles/MainPage.module.css'
+import styles from './styles/MainPage.module.scss'
+import React, { useRef } from 'react';
 const MainPage = () => {
-
+  const descriptionRef = useRef<HTMLDivElement | null>(null);
+  const bottomBlockRef = useRef<HTMLDivElement | null>(null);
   const updateHeight = () => {
     const ScreenHeight = window.innerHeight;
     document.documentElement.style.setProperty(
@@ -13,27 +16,66 @@ const MainPage = () => {
   };
 
   updateHeight();
+  const handleMouseEnterDescription = () => {
+    if (bottomBlockRef.current) {
+      bottomBlockRef.current.style.filter = 'blur(20px)';
+      bottomBlockRef.current.style.transition = '0.5s ease-in-out';
+      bottomBlockRef.current.style.transform = 'scale(0.9)';
+      bottomBlockRef.current.style.opacity = '0.5';
+    }
+  };
 
+  const handleMouseLeaveDescription = () => {
+    if (bottomBlockRef.current) {
+      bottomBlockRef.current.style.filter = '';
+      bottomBlockRef.current.style.transform = '';
+      bottomBlockRef.current.style.opacity = '';
+    }
+  };
+
+  const handleMouseEnterBottomBlock = () => {
+    if (descriptionRef.current) {
+      descriptionRef.current.style.filter = 'blur(20px)';
+      descriptionRef.current.style.transition = '0.5s ease-in-out';
+      descriptionRef.current.style.transform = 'scale(0.9)';
+      descriptionRef.current.style.opacity = '0.5';
+    }
+  };
+
+  const handleMouseLeaveBottomBlock = () => {
+    if (descriptionRef.current) {
+      descriptionRef.current.style.filter = '';
+      descriptionRef.current.style.transform = '';
+      descriptionRef.current.style.opacity = '';
+    }
+  };
   return (
     <>
 <Header/>
 <main className={styles.main_container}>
   <div className={styles.grid_container}>
   <MainPageSlider ClassName={styles.slider} />
-  <div className={styles.description}> 
+  <div 
+  className={styles.description}
+   ref={descriptionRef}
+   onMouseEnter={handleMouseEnterDescription}
+   onMouseLeave={handleMouseLeaveDescription}> 
     <h1 className={styles.descriptionHead}>Кто мы?</h1>
    <p className={styles.descriptionPar}>
    Мы небольшая группа программистов, которая хочет облегчить и усовершенствовать представление о тестах.
    Мы хотим чтобы проходить тесты было не скучно и их создание не занимало много времени.
     </p> </div>
-  <div className={styles.bottom_block}>
+  <div className={styles.bottom_block}
+  ref={bottomBlockRef}
+  onMouseEnter={handleMouseEnterBottomBlock}
+  onMouseLeave={handleMouseLeaveBottomBlock}>
     <h1 className={styles.BotBlockHead}>О проекте</h1>
 <p className={styles.BotBlockPar}> Наш проект даёт возможность учителям, преподавателям, компаниям и т.п. облегчить работа с тестами. На сайте есть возможность не только создавать тесты в редакторе ну и загружать их из Word’а что облегчит работу по переносу старых тестов на наш более удобный и практичный проект. Но это ещё не все чтобы увидеть на что способен наш сайт вы можете пройти тест который создали мы и убедиться что мы не врём.</p>
   </div>
   </div>
   <Benefits/>
 </main>
-
+{/* <Footer/> */}
     </>
   )
 }
