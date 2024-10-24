fd4116c0d592d54ff02a211f8f6e610c076b7cd0
m4kyv <braun-erik@inbox.ru>
2024-10-24 21:14:09 +0200

    Update RegPage.tsx

--- frontend/src/pages/RegPage.tsx
+++ frontend/src/pages/RegPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import styles from './styles/AuthPage.module.css';
+import styles from './styles/AuthPage.module.scss';
 
 interface FormData {
 
