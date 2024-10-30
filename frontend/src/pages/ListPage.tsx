99ea57b76ca381a9773b9f86907381873bc8bec3
Sk1nys <vkirill680@gmail.com>
2024-10-30 22:03:47 +0300

    AdaptivStrikeVer1337

--- frontend/src/pages/ListPage.tsx
+++ frontend/src/pages/ListPage.tsx
@@ -1,13 +1,8 @@
 //import React from 'react'
 import styles from './styles/ListPage.module.scss'
 const ListPage = () => {
-  return (
-    <div className={styles.jopaaa}>
-    <div className={styles.box}>
-    <div className={styles.vertical}>Welcome to <strong>The Box!</strong></div>
-  </div>
-  </div>
-  )
+null
+
 }
 
 export default ListPage
