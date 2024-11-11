e959c84ec672566862a975db573e2002760dc6f6
Sk1nys <vkirill680@gmail.com>
2024-11-11 21:54:00 +0300

    $_$

---
+++ frontend/src/Components/ForTests/InputTx/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from "../InputTx/InputTx.module.css"
+const InputTx = () => {
+    return (
+        <input type="text" className={styles.inputs}/>
+    )
+  }
+  export default InputTx
---
+++ frontend/src/Components/ForTests/RadioTx/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from "../RadioTx/RadioTx.module.scss"
+const RadioTx = () => {
+    return (
+      null
+    )
+  }
+  export default RadioTx
