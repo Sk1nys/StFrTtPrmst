a44cbc239bdb4051c430c9adfe626e9103aa7f8e
Sk1nys <vkirill680@gmail.com>
2025-01-14 22:16:21 +0300

    ProfileProtorype

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,7 +1,9 @@
 import React, { FC, useEffect, useState } from 'react';
 import { useCookies } from 'react-cookie';
 import axios from 'axios';
+import styles from './styles/ProfilePage.module.scss';
 import CryptoJS from 'crypto-js';
+import ButtonSquish from '../Components/Buttons/ButtonSquish';
 
 interface DataItem {
   id: number;
@@ -93,16 +95,17 @@ const ProfilePage: FC = () => {
 
   return (
     <div>
-      <button onClick={handleExit}>Выйти</button>
+      <div className={styles.header}><ButtonSquish>НАЗАД</ButtonSquish><h1>ЛИЧНЫЙ КАБИНЕТ</h1><button className={styles.btn} onClick={handleExit}><ButtonSquish>Выйти</ButtonSquish></button></div>
+      <div className={styles.container}>
       {data.length > 0 ? (
-        <div>
+        <div className={styles.result}>
           <h2>Результаты тестов</h2>
           {data.map((item) => (
-            <div key={item.id} style={{ marginBottom: '1em' }}>
+            <div className={styles.sell} key={item.id} style={{ marginBottom: '1em' }}>
               <p>{item.test.title}</p>
               <p>баллы: {item.score}</p>
               <p>максимум баллов: {item.total_score}</p>
-              <hr />
+              
             </div>
           ))}
         </div>
@@ -111,19 +114,20 @@ const ProfilePage: FC = () => {
       )}
 
       {additionalData.length > 0 ? (
-        <div>
-          <h2>Ваши тести</h2>
+        <div className={styles.mytest}>
+          <h2>Ваши тесты</h2>
           {additionalData.map((item) => (
-            <div key={item.id} style={{ marginBottom: '1em' }}>
+            <div className={styles.sell} key={item.id} style={{ marginBottom: '1em' }}>
               <p>{item.title}</p> {/* Отображаем только заголовок теста */}
               <p>Дата: {item.data}</p> {/* Отображаем дату теста */}
-              <hr />
+              
             </div>
           ))}
         </div>
       ) : (
         <div>Вы не создавали тест</div>
       )}
+    </div>
     </div>
   );
 };
