aa73a93d6362175a0b3b9d4b03c6954f5454d3bc
Sk1nys <vkirill680@gmail.com>
2025-01-20 21:48:10 +0300

    ScEp

--- frontend/src/pages/SecondEpisode.tsx
+++ frontend/src/pages/SecondEpisode.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import styles from './styles/SecondEpisode.module.scss'
+import { Link } from 'react-router-dom'
+const changeIn = ()=>{
+  let i = document.querySelector("input")
+  if(i.value=="left:50px;"){
+    i.style.left = "50px"
+    console.log("L.... S......")
+    localStorage.setItem("nextStage", "/thirdepisode");
+    
+  }
+}
 const SecondEpisode = () => {
   return (
-    <div>SecondEpisode</div>
+    <div className={styles.main}>
+        <h2>Тут темно надо включть свет</h2>
+        <h3>Вот так то лучше а теперь надо починить Input мне нужен текст</h3>
+        <label>.-.. . ..-. - ---... ..... ----- .--. -..- -.-.-.</label>
+        <input className={styles.secret} type="date" onChange={changeIn.bind(this)}/>
+        
+    </div>
   )
 }
 
